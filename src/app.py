b05fa6579592d541b58efdc963990e574ae286b4
IML <shin10256@gmail.com>
2021-01-29 20:08:28 +0900

    restruction2

--- src/app.py
+++ src/app.py
@@ -7,30 +7,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template(
-		'index.html', 
-		client_id=CLIENT_ID, 
-		redirect_uri=REDIRECT_URI
-	)
+    return render_template(
+        'index.html', 
+        client_id=CLIENT_ID, 
+        redirect_uri=REDIRECT_URI
+    )
 
 
 @app.route("/oauth")
 def oauth():
-	code = str(request.args.get('code'))
-	return authorize(code)
+    code = str(request.args.get('code'))
+    return authorize(code)
 
 
 @app.route("/oauth/refresh", methods=['POST'])
 def oauth_refesh():
-	refresh_token = request.get_json()['refresh_token']
-	return refresh(refresh_token)
+    refresh_token = request.get_json()['refresh_token']
+    return refresh(refresh_token)
 
 
 @app.route("/oauth/userinfo")
 def oauth_userinfo():
-	bearer_token = request.headers['Authorization']
-	return get_userinfo(bearer_token)
+    bearer_token = request.headers['Authorization']
+    return get_userinfo(bearer_token)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
--- src/oauth.py
+++ src/oauth.py
@@ -3,49 +3,46 @@ from config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 AUTH_SERVER = "https://kauth.kakao.com%s"
 API_SERVER = "https://kapi.kakao.com%s"
+DEFAULT_HEADER = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Cache-Control": "no-cache",
+}
 
 
 def authorize(code):
-	return requests.post(
-		url=AUTH_SERVER % "/oauth/token", 
-		headers={
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Cache-Control": "no-cache",
-		},
-		data={
-			"grant_type": "authorization_code",
-			"client_id": CLIENT_ID,
-			"client_secret": CLIENT_SECRET,
-			"redirect_uri": REDIRECT_URI,
-			"code": code,
-		}, 
-	).json()
+    return requests.post(
+        url=AUTH_SERVER % "/oauth/token", 
+        headers=DEFAULT_HEADER,
+        data={
+            "grant_type": "authorization_code",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "redirect_uri": REDIRECT_URI,
+            "code": code,
+        }, 
+    ).json()
 
 
 def refresh(refresh_token):
-	return requests.post(
-		url=AUTH_SERVER % "/oauth/token", 
-		headers={
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Cache-Control": "no-cache",
-		},
-		data={
-			"grant_type": "refresh_token",
-			"client_id": CLIENT_ID,
-			"client_secret": CLIENT_SECRET,
-			"refresh_token": refresh_token,
-		}, 
-	).json()
+    return requests.post(
+        url=AUTH_SERVER % "/oauth/token", 
+        headers=DEFAULT_HEADER,
+        data={
+            "grant_type": "refresh_token",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "refresh_token": refresh_token,
+        }, 
+    ).json()
+
 
 def get_userinfo(bearer_token):
-	return requests.post(
-		url=API_SERVER % "/v2/user/me", 
-		headers={
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Cache-Control": "no-cache",
-			"Authorization": bearer_token
-		},
-		data={
-			#"property_keys":'["kakao_account.profile_image_url"]'
-		}
-	).json()
+    return requests.post(
+        url=API_SERVER % "/v2/user/me", 
+        headers={
+            **DEFAULT_HEADER,
+            **{"Authorization": bearer_token}
+        },
+        #"property_keys":'["kakao_account.profile_image_url"]'
+        data={}
+    ).json()
